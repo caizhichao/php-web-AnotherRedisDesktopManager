188437258c27b526a88b1c1b691b6360518fda7a
蔡志超 <caizhichao@xmpaoyou.com>
2022-06-04 12:25:40 +0800

    升级element-ui依赖，兼容提示报错问题

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import ElementUI from 'element-ui';
+import ElementUI, { Message } from 'element-ui';
 import App from './App.vue';
 import 'font-awesome/css/font-awesome.css';
 import '../static/theme/chalk/index.css';
@@ -19,6 +19,29 @@ Vue.prototype.$util = util;
 Vue.prototype.$storage = storage;
 Vue.prototype.$shortcut = shortcut;
 Vue.prototype.$webApi = webApi;
+
+Message.success = (function () {
+  const tmp = Message.success;
+  return function (v) {
+    if (typeof v === 'object') {
+      v = Object.assign(v, { type: 'success' });
+      return Message(v);
+    }
+    return tmp(v);
+  };
+}());
+
+Message.error = (function () {
+  const tmp = Message.error;
+  return function (v) {
+    if (typeof v === 'object') {
+      v = Object.assign(v, { type: 'error' });
+      return Message(v);
+    }
+    return tmp(v);
+  };
+}());
+
 Vue.use(ElementUI, { size: 'small' });
 Vue.config.productionTip = false;
 
